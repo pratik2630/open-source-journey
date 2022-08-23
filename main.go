aa861d5a5beb03643bf77d699685277bdf863561
pratik2630 <pratikkorgaonkar26@gmail.com>
2022-08-23 20:53:55 +0530

    Added use of variable

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
 	fmt.Println("Hello world")
-	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Welcome to our ", conferenceName, " booking application")
+	fmt.Println("We have total of", conferenceTickets, "and ", remainingTickets, "are still remaiing")
 	fmt.Println("Get your tickets here to attend")
+
+	fmt.Println(conferenceName)
 }
